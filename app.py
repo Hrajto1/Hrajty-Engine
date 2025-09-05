9c204ed7f90665f2b2791531eb93882af271a548
Hrajto1 <135441727+Hrajto1@users.noreply.github.com>
2025-09-05 20:11:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,6 @@ def suggest():
     return jsonify(suggestions)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Render poskytne PORT, fallback na 5000 lokálně
+    app.run(host="0.0.0.0", port=port)
+
